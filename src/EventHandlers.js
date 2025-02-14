57b3f032dea36b4b7bbb61915efc097ba832871e
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-02-14 10:39:30 +0200

    Display error if result undefined

--- src/EventHandlers.js
+++ src/EventHandlers.js
@@ -1,11 +1,17 @@
 import { searchInput } from "./DOMElements";
 import { search } from "./ExtractData";
+import { updateWeather } from "./DisplayController";
 
 async function handleSearch(e) {
   e.preventDefault();
   const city = searchInput.value;
   searchInput.value = "";
-  const result = await search(city);
+  const result = await search(city)
+  if (!result){
+    alert("Error. City not found. Please try again.")
+    return
+  }
+  updateWeather(result.cityData, result.currentConditions);
   console.log(result.cityData);
   console.log(result.currentConditions);
 }
