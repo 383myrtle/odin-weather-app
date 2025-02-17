2e6b1faac895d6cd35ed33979dbdc8724bd668bd
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-02-17 11:30:54 +0200

    Add night weather icons

--- src/DisplayController.js
+++ src/DisplayController.js
@@ -10,15 +10,19 @@ import {
 import cloudy from "./assets/cloudy.svg";
 import rainy from "./assets/rainy.svg";
 import thunderstorm from "./assets/thunderstorm.svg";
-import partlyCloudy from "./assets/partly_cloudy.svg";
-import sunny from "./assets/sunny.svg";
+import partlyCloudyDay from "./assets/partly_cloudy.svg";
+import clearDay from "./assets/sunny.svg";
+import partlyCloudyNight from "./assets/partly_cloudy_night.svg";
+import clearNight from "./assets/clear_night.svg";
 
 const iconMap = {
   cloudy: cloudy,
   rain: rainy,
   thunderstorm: thunderstorm,
-  "partly-cloudy-day": partlyCloudy,
-  "clear-day": sunny,
+  "partly-cloudy-day": partlyCloudyDay,
+  "clear-day": clearDay,
+  "partly-cloudy-night": partlyCloudyNight,
+  "clear-night": clearNight,
 };
 
 const updateWeather = function (cityData, currentConditions, forecast) {
