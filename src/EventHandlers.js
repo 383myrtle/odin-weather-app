d85f63e21206fb9aa8eb8494d3904577a426f7c9
383myrtle <89387525+383myrtle@users.noreply.github.com>
2025-02-18 10:50:14 +0200

    Re-render current city on unit change

--- src/EventHandlers.js
+++ src/EventHandlers.js
@@ -9,20 +9,25 @@ async function handleSearch(e) {
   e.preventDefault();
   currentCity = searchInput.value;
   searchInput.value = "";
-  const result = await search(currentCity, getCurrentUnitGroup())
-  if (!result){
-    alert("Error. City not found. Please try again.")
-    return
+  await renderCurrentCity();
+}
+
+async function changeUnitGroup(e) {
+  const unitGroup = e.currentTarget.value;
+  setCurrentUnitGroup(unitGroup);
+  await renderCurrentCity();
+}
+
+async function renderCurrentCity() {
+  const result = await search(currentCity, getCurrentUnitGroup());
+  if (!result) {
+    alert("Error. City not found. Please try again.");
+    return;
   }
   updateWeather(result.cityData, result.currentConditions, result.forecast);
   console.log(result.cityData);
   console.log(result.currentConditions);
   console.log(result.forecast);
-}
-
-function changeUnitGroup(e){
-  const unitGroup = e.currentTarget.value;
-  setCurrentUnitGroup(unitGroup);
 }
 
 export { handleSearch, changeUnitGroup };
